1962f97341d7863e84ce71f09745d23c8be66035
Ayooluwa Oyewo <ayooluwaoyewo@gmail.com>
2020-01-08 17:49:37 +0100

    updating app with shop page, collection preview and collection item component

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,50 +1,13 @@
 import React, { Component } from 'react'
 import MenuItem from '../menu-item/menu-item.component';
 import './directory.styles.scss'
-
+import SECTIONS from './directory.data'
 class Directory extends Component {
     constructor(){
         super();
 
         this.state = {
-            sections: [
-                {
-                  title: 'hats',
-                  imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
-                  id: 1,
-                  linkUrl: 'hats'
-                },
-                {
-                  title: 'jackets',
-                  imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
-                  id: 2,
-                  linkUrl: ''
-
-                },
-                {
-                  title: 'sneakers',
-                  imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
-                  id: 3,
-                  linkUrl: ''
-
-                },
-                {
-                  title: 'womens',
-                  imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
-                  size: 'large',
-                  id: 4,
-                  linkUrl: ''
-
-                },
-                {
-                  title: 'mens',
-                  imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
-                  size: 'large',
-                  id: 5,
-                  linkUrl: ''
-
-                }
-              ]
+            sections: SECTIONS
         }
     }
 
