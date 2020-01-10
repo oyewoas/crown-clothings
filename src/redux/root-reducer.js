a259b672c8c41aa31ef9027a93e18e1164245c2b
Ayooluwa Oyewo <ayooluwaoyewo@gmail.com>
2020-01-10 18:16:09 +0100

    added cart icon and dropdown and also added cart reducer

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import userReducer from "./user/user.reducer";
+import cartReducer from "./cart/cart.reducer";
 
 // represent all the state(reducer) for the App
 
 export default combineReducers({
-    user: userReducer
+    user: userReducer,
+    cart: cartReducer
 })
