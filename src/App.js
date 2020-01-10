415be93ec651273b76658f73eca9d85656fa634e
Ayooluwa Oyewo <ayooluwaoyewo@gmail.com>
2020-01-10 17:26:34 +0100

    added redux and implemented user reducer and actions to header and app components

--- src/App.js
+++ src/App.js
@@ -6,56 +6,54 @@ import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import { auth, CreateUserProfileDocument } from './firebase/firebase.utils';
+import { connect } from 'react-redux';
+import { setCurrentUser } from './redux/user/user.actions';
 
 class App extends Component {
-  constructor(){
-    super()
+  unsubscribeFromAuth = null;
 
-    this.state = {
-      currentUser: null
-    }
-  }
-  unsubscribeFromAuth = null
-  componentDidMount(){
+  componentDidMount() {
+    const { setCurrentUser } = this.props;
+
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
-      
       if (userAuth) {
-        const userRef = await CreateUserProfileDocument(userAuth)
+        const userRef = await CreateUserProfileDocument(userAuth);
 
         userRef.onSnapshot(snapShot => {
-          this.setState({
-            currentUser : {
-              id: snapShot.id,
-              ...snapShot.data()
-            }
-          })
-
-        })
-      } else {
-        this.setState({
-          currentUser: userAuth
-        })
+          setCurrentUser({
+            id: snapShot.id,
+            ...snapShot.data()
+          });
+        });
       }
-    })
+
+      setCurrentUser(userAuth);
+    });
   }
 
-  componentWillUnmount(){
-    this.unsubscribeFromAuth()
+  componentWillUnmount() {
+    this.unsubscribeFromAuth();
   }
-  render(){
-    return ( 
+
+  render() {
+    return (
       <div>
-        <Header currentUser={this.state.currentUser}/> 
+        <Header />
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={ShopPage} />
           <Route path='/signin' component={SignInAndSignUpPage} />
-  
-        </Switch> 
+        </Switch>
       </div>
-    )
+    );
   }
-  
 }
 
-export default App;
+const mapDispatchToProps = dispatch => ({
+  setCurrentUser: user => dispatch(setCurrentUser(user))
+});
+
+export default connect(
+  null,
+  mapDispatchToProps
+)(App);
---
+++ src/redux/root-reducer.js
@@ -0,0 +1,8 @@
+import { combineReducers } from "redux";
+import userReducer from "./user/user.reducer";
+
+// represent all the state(reducer) for the App
+
+export default combineReducers({
+    user: userReducer
+})
