e469e74cda70e7d04dbafd4916b3e60b0ae736f9
Ayooluwa Oyewo <ayooluwaoyewo@gmail.com>
2020-01-16 17:06:35 +0100

    moved directory and shop data to reducer and created respective selectors and updated corresponding ux flow for directory/shop

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,27 +1,23 @@
-import React, { Component } from 'react'
+import React from 'react'
 import MenuItem from '../menu-item/menu-item.component';
 import './directory.styles.scss'
-import SECTIONS from './directory.data'
-class Directory extends Component {
-    constructor(){
-        super();
+import { connect } from 'react-redux';
+import { selectDirectorySections } from '../../redux/directory/director.selector';
+import { createStructuredSelector } from 'reselect';
 
-        this.state = {
-            sections: SECTIONS
+
+const Directory = ({sections}) => (
+    <div className="directory-menu">
+        {
+            sections.map(({id, ...otherSectionProps}) => (
+                <MenuItem key={id} {...otherSectionProps}/>
+                ))
         }
-    }
+    </div>
+)
 
-    render(){
-        return(
-            <div className="directory-menu">
-                {
-                    this.state.sections.map(({id, ...otherSectionProps}) => (
-                        <MenuItem key={id} {...otherSectionProps}/>
-                    ))
-                }
-            </div>
-        )
-    }
-}
+const mapStateToProps = createStructuredSelector({
+    sections: selectDirectorySections
+})
 
-export default Directory;
+export default connect(mapStateToProps)(Directory);
