09ac60ed192c9149f1a706fa7589889ca8143dc4
Ayooluwa Oyewo <ayooluwaoyewo@gmail.com>
2020-06-15 12:47:00 +0100

    using redux thunk to fire multiple actions as functions

--- src/redux/shop/shop.selector.js
+++ src/redux/shop/shop.selector.js
@@ -1,19 +1,32 @@
-import { createSelector } from "reselect";
+import {
+    createSelector
+} from "reselect";
 
 const selectShop = state => state.shop;
 
 export const selectCollections = createSelector(
     [selectShop],
-    (shop) => shop.collections 
+    (shop) => shop.collections
 );
 
 export const selectCollectionsForPreview = createSelector(
     [selectCollections],
     collections => collections ? Object.keys(collections).map(key => collections[key]) : []
-  );
-  
-export const selectCollection = collectionUrlParam => 
+);
+
+export const selectCollection = collectionUrlParam =>
     createSelector(
         [selectCollections],
         collections => (collections ? collections[collectionUrlParam] : null)
+    )
+
+export const selectIsCollectionFetching = createSelector(
+    [selectShop],
+    shop => shop.isFetching
+)
+
+export const selectIsCollectionLoaded = createSelector(
+    [selectShop],
+    shop => !!shop.collections
+
 )
